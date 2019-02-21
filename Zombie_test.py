bc3da1506bd1e3e2d63b266496a8d3d0c6598483
Anthony DiRusso <ossurid@gmail.com>
2019-02-21 10:55:16 -0500

    gives `goblin` `health` and `power` attributes

--- Zombie_test.py
+++ Zombie_test.py
@@ -6,13 +6,13 @@ from Zombie_Attack import Goblin
 
 test = Character("test")
 test_Hero = Hero("Hero", 10, 5)
-test_Goblin = Goblin("Goblin" 6, 2)
+test_Goblin = Goblin("Goblin", 6, 2)
 
 print(test.name)
 print(test_Hero.name)
-print(test_Hero.health)
-print(test_Hero.power)
+print("The hero has %d, health." % (test_Hero.health))
+print("The hero does %d damage in attack." % (test_Hero.power))
 print(test_Goblin.name)
-print("The goblin has %d, health" % (test_Goblin.health))
-print("The goblin does %d, damage in attack." % (test_Goblin.power))
+print("The goblin has %d, health." % (test_Goblin.health))
+print("The goblin does %d damage, in attack." % (test_Goblin.power))
 
